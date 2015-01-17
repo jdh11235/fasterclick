3a352a76aadd19c58c8d33723429937531e3fac9
Jonathan Herman <jdh11235@gmail.com>
2015-01-16 21:08:06 -0600

    readme formatting tweaks

--- fasterclick.js
+++ fasterclick.js
@@ -13,12 +13,13 @@
 			var uid = Fasterclick.latest_uid + 1;
 			Fasterclick.latest_uid++;
 
-			//construct Function() element event wrapper that calls cache with uid
+			//construct Function() event wrapper that calls handler with uid
 			var touch_wrapper = Function('event', 'Fasterclick.touchHandler(event, "' + uid + '")');
 
 			var click_wrapper = Function('event', 'Fasterclick.clickHandler(event, "' + uid + '")');
 
-			//store element callback and counter info in cache using uid
+			//add references
+			Fasterclick.elements[uid] = element;
 			Fasterclick.callbacks[uid] = callback;
 			Fasterclick.touch_counters[uid] = 0;
 
@@ -26,20 +27,43 @@
 			element.addEventListener('touch', touch_wrapper);
 			element.addEventListener('click', click_wrapper);
 
+			//for use with Fasterclick.cancel(uid);
+			return uid;
+		},
+
+		cancel: function(uid) {
+			Fasterclick.elements[uid].removeEventListener(Fasterclick.touch_events[uid]);
+			Fasterclick.elements[uid].removeEventListener(Fasterclick.click_events[uid]);
+
+			//remove references
+			Fasterclick.elements[uid] = null;
+			Fasterclick.callbacks[uid] = null;
+			Fasterclick.touch_events[uid] = null;
+			Fasterclick.click_events[uid] = null;
+			Fasterclick.touch_counters[uid] = null;
 		},
 
 		//event handlers
 		touchHandler: function(event, uid) {
 			//TODO
+			console.log(event, uid);
 		},
 
 		clickHandler: function(event, uid) {
 			//TODO
+			console.log(event, uid);
 		},
 
-		//reference caches
+		//FUTURE: move the following code into Fasterclick.cache
+
+		//reference caches for event handlers
 		callbacks: [],
 		touch_counters: [],
+
+		//reference caches for Fasterclick.cancel()
+		elements: [],
+		touch_events: [],
+		click_events: [],
 
 		//misc. helpers
 		latest_uid: 0
