5eb0cfa3e883bcaa48922ef479cdeaa0b5904d16
Jonathan Herman <jdh11235@gmail.com>
2015-01-16 20:10:16 -0600

    use Function() constructor to call event handlers

--- fasterclick.js
+++ fasterclick.js
@@ -7,11 +7,17 @@
 		attach: function(element, callback) {
 
 			//generate uid to store element, callback, touch_counter in cache
-			var uid = 1;
+			var uid = Fasterclick.latest_uid + 1;
+			Fasterclick.latest_uid++;
 
 			//construct Function() element event wrapper that calls cache with uid
-			var touch_wrapper = Function('');
-			var click_wrapper = Function('');
+			var touch_wrapper = Function('event', 'Fasterclick.touchHandler(event, "' + uid + '")');
+
+			var click_wrapper = Function('event', 'Fasterclick.clickHandler(event, "' + uid + '")');
+
+			//store element callback and counter info in cache using uid
+			Fasterclick.callbacks[uid] = callback;
+			Fasterclick.touch_counters[uid] = 0;
 
 			//attach event wrappers to element
 			element.addEventListener('touch', touch_wrapper);
@@ -19,8 +25,16 @@
 
 		},
 
+		//event handlers
+		touchHandler: function(event, uid) {
+			//TODO
+		},
+
+		clickHandler: function(event, uid) {
+			//TODO
+		},
+
 		//reference caches
-		elements: [],
 		callbacks: [],
 		touch_counters: [],
 
