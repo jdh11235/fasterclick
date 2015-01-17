0a837c6d0c68225bf486db469fdabd81b93d9f29
Jonathan Herman <jdh11235@gmail.com>
2015-01-16 19:29:30 -0600

    better roadmap

--- fasterclick.js
+++ fasterclick.js
@@ -6,12 +6,15 @@
 		//main method
 		attach: function(element, callback) {
 
+			//add Function() wrapper to element event listener that calls reference cache
+			//generate uid to store element, callback, touch_counter in caches
+
 		},
 
 		//reference caches
-		elements: {},
-		callbacks: {},
-		touch_counters: {}
+		elements: [],
+		callbacks: [],
+		touch_counters: []
 
 	};
 })();
