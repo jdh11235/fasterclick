5780416ada3be90c319057e4bf21dd49be474fcf
Jonathan Herman <jdh11235@gmail.com>
2015-01-16 19:39:22 -0600

    develop idea

--- fasterclick.js
+++ fasterclick.js
@@ -7,14 +7,25 @@
 		attach: function(element, callback) {
 
 			//generate uid to store element, callback, touch_counter in cache
+			var uid = 1;
+
 			//construct Function() element event wrapper that calls cache with uid
+			var touch_wrapper = Function('');
+			var click_wrapper = Function('');
+
+			//attach event wrappers to element
+			element.addEventListener('touch', touch_wrapper);
+			element.addEventListener('click', click_wrapper);
 
 		},
 
 		//reference caches
 		elements: [],
 		callbacks: [],
-		touch_counters: []
+		touch_counters: [],
+
+		//misc. helpers
+		latest_uid: 0
 
 	};
 })();
